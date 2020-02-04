0d5a7d3fc338b60a129504f1c441b4518060db8e
Norman Lorrain <normanlorrain@gmail.com>
2020-02-03 19:36:39 -0700

    Test creates a file and handles result.

--- tests/test_pyharu.py
+++ tests/test_pyharu.py
@@ -1,21 +1,31 @@
 import pytest
-
+import sys
+from ctypes import *
 from pathlib import Path
 
 from pyharoo.haru import *
+from pyharoo.haru.hpdf_errorcode import error_detail
 
 TEST_DIR = Path(__file__).parent.absolute() / 'output'
 TEST_DIR.mkdir(parents=True, exist_ok=True)
 
 
+@HPDF_Error_Handler(None, HPDF_UINT, HPDF_UINT, c_void_p)
+def error_handler (error_no, detail_no, user_data):
+    print (f"ERROR: {error_detail[error_no]}, detail_no={detail_no}")
+    return
+
+error_handler = None
+
 
 def test_emptyPDF():
-    error_handler = None 
-    NULL = None
     pdf = HPDF_New (error_handler, NULL)
     page_1 = HPDF_AddPage (pdf)
     outfilename = bytes(TEST_DIR/"test.pdf")
     print(outfilename)
-    HPDF_SaveToFile (pdf, outfilename)
-
+    res = HPDF_SaveToFile (pdf, outfilename)
+    if res: 
+        detail = HPDF_GetErrorDetail()
+        print(detail)
+        raise Exception( error_detail[res] )
     HPDF_Free (pdf)
