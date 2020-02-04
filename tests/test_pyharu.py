c16fb803fa9cd764f661f7490b2688c1ba09d212
Norman Lorrain <normanlorrain@gmail.com>
2020-02-03 18:41:33 -0700

    Tests output to ignored directory.

--- tests/test_pyharu.py
+++ tests/test_pyharu.py
@@ -1,12 +1,21 @@
+import pytest
+
+from pathlib import Path
+
 from pyharoo.haru import *
 
+TEST_DIR = Path(__file__).parent.absolute() / 'output'
+TEST_DIR.mkdir(parents=True, exist_ok=True)
 
 
-error_handler = None 
-NULL = None
-pdf = HPDF_New (error_handler, NULL)
-page_1 = HPDF_AddPage (pdf)
 
-HPDF_SaveToFile (pdf, r"test.pdf")
+def test_emptyPDF():
+    error_handler = None 
+    NULL = None
+    pdf = HPDF_New (error_handler, NULL)
+    page_1 = HPDF_AddPage (pdf)
+    outfilename = bytes(TEST_DIR/"test.pdf")
+    print(outfilename)
+    HPDF_SaveToFile (pdf, outfilename)
 
-HPDF_Free (pdf)
+    HPDF_Free (pdf)
